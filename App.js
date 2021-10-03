3b42587614fc1d22e18867a9a26aebceb93e4b36
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-10-02 22:25:15 -0300

    App

--- App.js
+++ App.js
@@ -1,23 +1,87 @@
-import React, { Component } from 'react';
-import {StyleSheet,View,Text} from 'react-native';
-import C1 from './componentes/comp1'
-import Estilos from './estilos/Estilos.js'
+import * as React from 'react';
+import {View,StyleSheet,Text,Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Guias = createBottomTabNavigator();
+
+function TelaHome({navigation}){
+  return(
+     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tela Home</Text>
+      <Text>Cursos</Text>
+     </View>
+  );
+}
+
+function TelaCanal({navigation}){
+  return(
+     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tela Canal</Text>
+      <Text>youtube.com/cursos</Text>
+     </View>
+  );
+}
+
+function TelaCursos({navigation}){
+    return(
+       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Tela Cursos</Text>
+       </View>
+    );
+  }
 
 export default function App1(){
   return (
-    <View style={Estilos.conteiner}>
-      <C1 curso="React Native" nota="10"/>
-      <C1 curso="C++" nota="10"/>
-      <C1 curso="Javascript" nota="10"/>
-      <C1 curso="C#" nota="10"/>
-      <Text style={Estilos.textoPadrao}>youtube.com</Text>
-      <Text style={Estilos.textoTitulo}>cursos.com.br</Text>
-    </View>
+    <NavigationContainer>
+      <Guias.Navigator initialRouteName="TelaHome">
+        <Guias.Screen
+          name="Home"
+          component={TelaHome}
+          options={{
+            title:'Tela Inicial',
+            headerStyle:{
+              backgroundColor:'#008'
+            },
+            headerTintColor:'#fff',
+            headerRight:()=>(
+              <Button
+                title='Cursos'
+                color='#000'
+                onPress={()=>alert('Botão Cursos Clicado')}
+              />
+            ),
+          }}
+        />
+        <Guias.Screen
+          name="Canal"
+          component={TelaCanal}
+          options={{
+            title:'Tela Canal',
+            headerStyle:{
+              backgroundColor:'#008'
+            },
+            headerTintColor:'#fff',
+            headerTitleStyle:{
+              fontWeight:'bold'
+            }
+          }}
+        />
+        <Guias.Screen
+          name="Cursos"
+          component={TelaCursos}
+          options={{title:'Cursos do Canal'}}
+        />
+      </Guias.Navigator>
+    </NavigationContainer>
   );
 };
 
+const estilos = StyleSheet.create({
 
+});
 /*
+//taaaa
 export default class App1 extends Component{
   render(){
     return(
@@ -26,5 +90,26 @@ export default class App1 extends Component{
       </View>
     );
   }
+}
+      <Image
+        source={require('./assets/logo.png')}
+        style={estilos.logo}
+      />
+
+
+Arrow Function 
+const dobro=n=>n*2
+const soma=(n1,n2)=>n1+n2
+
+      <Text>{dobro(2)}</Text>
+      <Text>{soma(2,4)}</Text>
+
+      <Button
+        title="Mostrar mensagem"
+        onPress={msg}
+        />
+
+        const msg=()=>{
+  Alert.alert('MSG','Curso de React Native')
 }
 */
