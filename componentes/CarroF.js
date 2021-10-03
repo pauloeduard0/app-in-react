504f612759c0ca3c6ff5c02e9b2d0982d20600e3
Paulo Eduardo <pauloedu.rocha@hotmail.com>
2021-10-02 22:52:45 -0300

    Liga e desliga carros

---
+++ componentes/CarroF.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import {View,Text,Switch} from 'react-native'
+
+export default function(props){
+    const [ligado,setLigado] = useState(false)
+    const toggleLigado=()=>setLigado(!ligado)
+    //const toggleLigado=()=>setLigado(previousState => !previousState)
+    return(
+        <View>
+            <Text>Carro:{props.nome} - Ligado:{ligado ? 'Sim' : 'Não'}</Text>
+            <Switch
+                trackColor={{false:'#777', true:'#8bf'}}
+                thumbColor={ligado ? '#00f' : '#444'}
+                value={ligado}
+                onValueChange={toggleLigado}
+            />
+        </View>
+    )
+}
